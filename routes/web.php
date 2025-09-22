0e90e66311a65adc797f23f4dcd96994396d4224
hervyola <hervyola23ti@mahasiswa.pcr.ac.id>
2025-09-22 08:00:55 +0700

    Project baru laravel

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\MahasiswaController;
+
+Route::get('/', function () {
+    return view('welcome hmm');
+});
+
+
+Route::get('/pcr', function(){
+    return 'Selamat Datang di Website Kampus Pcr!';
+});
+
+Route::get('/mahasiswa', function(){
+    return 'Selamat Datang di Kampus Pcr!';
+})->name('mahasiswa.show');
+
+Route::get('/nama/{param1}', function($param1){
+    return 'Nama Saya: '.$param1;
+});
+
+Route::get('/nim/{param1?', function($param1){
+    return 'Nim Saya: '.$param1;
+});
+
+Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
+
+Route::get('/about', function () {
+    return view('halaman-about');
+});
